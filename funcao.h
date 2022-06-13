1cf741749a61ee2a03c785216340f50b7b92a207
mcgema <65995291+mcgema@users.noreply.github.com>
2022-06-12 22:26:57 -0300

    f returns z+wi

--- funcao.h
+++ funcao.h
@@ -1,11 +1,15 @@
 #include<cmath>
+struct Imaginary {
+	float i;
+	float j;
+};
 
 class funcao {
 	float xmin, xmax, ymin, ymax;
 	int points;
 public:
 	funcao(float xm = -1, float xM = 1, float ym = -1, float yM =
-		1, int p = 10) {
+		1, int p = 100) {
 		xmin = xm;
 		ymin = ym;
 		xmax = xM;
@@ -13,11 +17,12 @@ public:
 		points = p;
 	}
 
-	float f(float r, float theta) {
-		float v[2];
-		v[0] = r * r * (cos(theta) * cos(theta) - sin(theta) * sin(theta));
-		v[1] = 2 * r * r * cos(theta) * sin(theta);
-		return v[2];
+	Imaginary f(float x, float y) {
+		float r, theta;
+		theta = atan(y / x);
+		r = x * x + y * y;
+		return { r * r * (cos(theta) * cos(theta) - sin(theta) * sin(theta)), 
+				2 * r * r * cos(theta) * sin(theta) };
 	}
 
 	void plota_funcao();
