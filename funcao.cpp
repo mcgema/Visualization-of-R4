9ba24b05363b070eb5400dc7390e117e51996e93
mcgema <65995291+mcgema@users.noreply.github.com>
2022-06-12 22:27:34 -0300

    updated for 4 dimension

--- funcao.cpp
+++ funcao.cpp
@@ -4,16 +4,16 @@ void funcao::plota_funcao() {
 	float x, y;
 	float dx = (xmax - xmin) / points;
 	float dy = (ymax - ymin) / points;
-	glColor3f(1.0f, 1.0f, 1.0f);
+	glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
 	x = xmin;
 	for (int i = 0; i < points; i++) {
 		y = ymin;
 		for (int j = 0; j < points; j++) {
 			glBegin(GL_LINE_LOOP);
-			glVertex3f(x, y, f(x, y));
-			glVertex3f(x + dx, y, f(x + dx, y));
-			glVertex3f(x + dx, y + dy, f(x + dx, y + dy));
-			glVertex3f(x, y + dy, f(x, y + dy));
+			glVertex4f(x, y, f(x, y).i, f(x,y).j);
+			glVertex4f(x + dx, y, f(x + dx, y).i, f(x + dx, y).j);
+			glVertex4f(x + dx, y + dy, f(x + dx, y + dy).i, f(x + dx, y + dy).j);
+			glVertex4f(x, y + dy, f(x, y + dy).i, f(x, y + dy).j);
 			glEnd();
 			y += dy;
 		}
