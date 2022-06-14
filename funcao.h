baa85e9f6b55503ef150fc5f28b5d0fe9708c26a
mcgema <65995291+mcgema@users.noreply.github.com>
2022-06-14 18:54:25 -0300

    updated input of functions to be functions

--- funcao.h
+++ funcao.h
@@ -1,4 +1,6 @@
-#include<cmath>
+#define _USE_MATH_DEFINES
+#include <cmath> 
+
 struct Quartenion {
 	float x;
 	float y;
@@ -7,26 +9,16 @@ struct Quartenion {
 };
 
 class funcao {
-	float xmin, xmax, ymin, ymax;
+	float rmin, rmax, thetamin, thetamax;
 	int points;
 public:
-	funcao(float xm = -1, float xM = 1, float ym = -1, float yM =
-		1, int p = 50) {
-		xmin = xm;
-		ymin = ym;
-		xmax = xM;
-		ymax = yM;
+	funcao(float rm = 0, float rM = 1, float tm = 0 , float tM = 2* M_PI, int p = 50) {
+		rmin = rm;
+		thetamin = tm;
+		rmax = rM;
+		thetamax = tM;
 		points = p;
 	}
-
-	Quartenion f(float x, float y) {
-		float r, theta;
-		theta = atan(y / x);
-		r = x * x + y * y;
-		return { x, y, r * r * (cos(theta) * cos(theta) - sin(theta) * sin(theta)),
-				2 * r * r * cos(theta) * sin(theta) };
-	}
-
-	void plota_funcao();
-	void draw_eixos_funcao();
+	void plota_funcao(Quartenion f(float, float));
+	void draw_eixos_funcao(Quartenion f(float, float));
 };
