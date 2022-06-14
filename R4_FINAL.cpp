c3e7805510d0f63821a817a0599e72d4fedb685d
mcgema <65995291+mcgema@users.noreply.github.com>
2022-06-14 18:46:58 -0300

    add functions

--- R4_FINAL.cpp
+++ R4_FINAL.cpp
@@ -2,58 +2,71 @@
 #include "funcao.h"
 #include <math.h>
 #include <iostream>
-
+using namespace std;
 
 float gamma[6] = { 0,0,0,0,0,0 };
 int flag = 0;
-
-float alpha = 40, beta = 60;
 float scale = 1.0;
 int xb, xm, yb, ym;
 
 funcao f;
 
+//lista de funcoes para serem plotadas
+Quartenion f1(float r, float theta) {
+	Quartenion q;
+	q.x = r * cos(theta);
+	q.y = r * sin(theta);
+	q.z = r * r * cos(2 * theta);
+	q.w = r * r * sin(2 * theta);
+	return q;
+}
 
+Quartenion f2(float r, float theta) {
+	return { r * cos(theta), r * sin(theta), r * cos(theta), 0 };
+}
+
+Quartenion f3(float r, float theta) {
+	int n = 5;
+	float p = log(n) * sin(theta);
+	return { r * cos(theta), r * sin(theta), r * cos(p), r * sin(p) };
+}
 
 void inicia_config()
 {
 	glClearColor(1, 1, 1, 1);
-	/*glLoadIdentity();*/
-	gluOrtho2D(0, 800, 800, 0);
-	//glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-	//glPushMatrix();
-	//glRotatef(alpha, 0.0, 0.0, 1.0);
+	gluOrtho2D(1, 1, 1, 1);
+	
 
-	/*glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	glOrtho(1, 1, 1, 1, 1, 1);
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-	glRotatef(alpha, 1, 0, 0);
-	glRotatef(beta, 0, 1, 0);
-	glScalef(scale, scale, scale);*/
+	glScalef(scale, scale, scale);
 }
+
+
 void redesenha()
 {
 	glClearColor(0.0, 0.0, 0.0, 0.0);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	//draw_eixos();
-	f.draw_eixos_funcao();
-	f.plota_funcao();
+	f.draw_eixos_funcao(f1); // parametro é funcao a ser plotada
+	f.plota_funcao(f1);
+	glFlush();
 	glutSwapBuffers();
+
 }
 
 void keyboard(unsigned char key, int x, int y)
 {
 	switch (key)
 	{
-	case 'S':
+	case 'q':
+	case 'Q':
 		scale += 0.2;
 		inicia_config();
 		redesenha();
 		break;
-	case 's':
+	case 'w':
+	case 'W':
 		scale -= 0.2;
 		inicia_config();
 		redesenha();
@@ -76,59 +89,37 @@ void keyboard(unsigned char key, int x, int y)
 	case '6':
 		flag = 5;
 		break;
-	case 'q':
-	case 'Q':
+	case 'a':
+	case 'A':
 		gamma[flag] -= 1;
 		inicia_config();
 		redesenha();
 		break;
-	case 'e':
-	case 'E':
+	case 's':
+	case 'S':
 		gamma[flag] += 1;
 		inicia_config();
 		redesenha();
 		break;
-	case "r":
-	case 'R':
+	case 'd':
+	case 'D':
 		for (int i = 0; i < 6; i++) {
 			gamma[i] = 0;
 		}
 		inicia_config();
 		redesenha();
-	}
-}
-void botao_mouse(int b, int state, int x, int y)
-{
-	switch (b) {
-	case GLUT_LEFT_BUTTON:
-		switch (state) {
-		case GLUT_DOWN:
-			xb = x;
-			yb = y;
-
-			break;
-		}
-	case GLUT_UP:
-		alpha = alpha + xm - xb;
-		beta = beta - ym + yb;
 		break;
 	}
-}
-void mov_mouse(int x, int y)
-{
-	xm = x;
-	ym = y;
-	alpha = alpha + xm - xb;
-	beta = beta - ym + yb;
-	inicia_config();
-	xb = xm;
-	yb = ym;
-	redesenha();
 }
 
 
 int main(int argc, char** argv)
 {
+	cout << "Guia para manusear o teclado:" << endl;
+	cout << "Os numeros 1, 2, 3, 4, 5, 6 em cojunto com a/A e s/S alteram o valor dos 6 angulos de euler" << endl;
+	cout << "a/A diminui o angulo em 1 grau e s/S aumenta o angulo em 1 grau " << endl;
+	cout << "d/D zera todos os angulos e devera ver o dominio da funcao" << endl;
+	cout << "q/Q aumenta o zoom no desenho e w/W diminui o zoom" << endl;
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
 	glutInitWindowSize(400, 400);
@@ -137,8 +128,5 @@ int main(int argc, char** argv)
 	inicia_config();
 	glutDisplayFunc(redesenha);
 	glutKeyboardFunc(keyboard);
-	glutMouseFunc(botao_mouse);
-	glutMotionFunc(mov_mouse);
-
 	glutMainLoop();
 }
